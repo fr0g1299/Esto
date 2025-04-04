6b5b8007538015cc06b444c7764c63f8845ef933
fr0g1299 <dodoloka1299@gmail.com>
2025-04-04 20:47:33 +0200

    Rename hideTabBar to useTabBarScrollEffect to better clarify it's a hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,7 @@ import { useEffect, useState } from "react";
 import { notificationsOutline, add } from "ionicons/icons";
 import "../styles/Home.css";
 import { useHistory } from "react-router";
-import { useTabBarScrollEffect } from "../hooks/hideTabBar";
+import { useTabBarScrollEffect } from "../hooks/useTabBarScrollEffect";
 
 import { db } from "../firebase";
 import { collection, getDocs, query, orderBy } from "firebase/firestore";
